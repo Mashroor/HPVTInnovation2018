40a280470a68157716be383deacffaaff4fd3d95
Mashroor Rashid <mashroor@sbcglobal.net>
2019-01-16 03:10:39 -0600

    Added initial test for LED, make sure that breadboarding and pinout work as intended for more LEDs to be added

---
+++ initArduinoMicro/firstLedTestArduino/firstLedTestArduino.ino
@@ -0,0 +1,11 @@
+int bLed = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(bLed, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(bLed, HIGH);
+}
